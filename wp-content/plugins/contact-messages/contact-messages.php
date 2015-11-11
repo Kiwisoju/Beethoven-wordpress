c8e946580c0d2b9854913e90892000b04f9ddf80
kiwisoju <glennjamesforrest@gmail.com>
2015-11-11 11:37:49 +0000

    Contact logging messages to table, displaying in WordPress Dashboard

---
+++ wp-content/plugins/contact-messages/contact-messages.php
@@ -0,0 +1,104 @@
+<?php
+
+/**
+ * Plugin Name: Contact Messages
+ * Version: 0.0.1
+ * Description: Plugin to save messages from contact form into a table, easily visible within WordPress Dashboard
+*/
+
+class ContactLogs{
+    private $db;
+
+    public function __construct(){
+     
+        global $wpdb;
+        $this->db = $wpdb;
+        
+        add_action('wp_ajax_some_action', array(&$this, 'ajax_some_action') );
+        add_action('wp_ajax_nopriv_some_action', array(&$this, 'ajax_some_action') );
+        add_action('wp_ajax_contact_log', array(&$this, 'ajax_contact_log') );
+        add_action('wp_ajax_nopriv_contact_log', array(&$this, 'ajax_contact_log') );
+        
+        add_action('admin_menu', array(&$this, 'register_menu_page') );
+        
+       
+    }
+    
+    public function register_menu_page(){
+        add_menu_page('Contact Logs', 'Contact Logs', 'edit_pages', 'contact logs', array(&$this, 'contact_logs') );
+    }
+    
+    public function contact_logs(){
+        $logs = $this->db->get_results('SELECT * FROM `contact_logs`');
+        include_once('_contact_logs.php');
+    }
+    
+    public function ajax_some_action(){
+        die(json_encode(array('message'=>'my hovercraft is full of EELS!!')));
+    }
+     
+    public function ajax_contact_log(){
+        $log = $_POST['log'];
+        if($log){
+            $log['ip'] = $_SERVER['REMOTE_ADDR'];
+            $result = $this->db->insert('contact_logs', $log);
+        }
+        die(json_encode(array('result' => $this->db->insert_id)));
+    }
+    /**
+     * This runs when the plugin is activated. Here we are
+     * creating a contact logs table. Info found on https://codex.wordpress.org/Creating_Tables_with_Plugins
+     */ 
+     
+    public function install(){
+        
+        $sql = "CREATE TABLE `contact_logs` (
+                    id int(11) NOT NULL AUTO_INCREMENT,
+                    name varchar(50) NOT NULL,
+                    email varchar(150) NOT NULL,
+                    message mediumtext NOT NULL,
+                    ip varchar(128) NOT NULL,
+                    date TIMESTAMP NOT NULL,
+                    UNIQUE KEY id (id)
+                ) ".$this->db->get_charset_collate();
+                
+        require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+        dbDelta($sql);
+    }
+    
+    /**
+     * This runs when the plugin is deactivated. Here we drop
+     * the logs table. If this is a likely plugin to be deactivated
+     * and reactivated, prompt user whether they wish to drop the table
+     * as all data will be dropped.
+     */
+     
+    public function uninstall(){
+        $sql = "DROP TABLE IF EXISTS `contact_logs`";
+        $this->db->query($sql);
+        
+    }
+    
+    public function init(){
+        wp_die(var_dump('oh hey'));
+    }
+}
+
+/**
+ * This makes sure no name collisions occur and only one 
+ * instantiation of the class occurs. This is better for
+ *  more in depth examples, better for uses such as public
+ * releases etc.
+ */
+ 
+if(class_exists('ContactLogs')){
+    if(!isset($contactLogs)){
+        $contactLogs = new ContactLogs();
+    }
+}
+
+
+//This hooks onto the action of activating your plugin
+register_activation_hook(__FILE__, array(&$contactLogs, 'install') );
+//This hooks onto the action of deactivating your plugin
+register_deactivation_hook(__FILE__, array(&$contactLogs, 'uninstall') );
--- wp-content/themes/industry/_contact_form.php
+++ wp-content/themes/industry/_contact_form.php
@@ -1,12 +1,42 @@
 <h2>Leave a Comment</h2>
-<form>
+<form id="contact-form">
     <div class="form-group">
-        <input type="text" class="form-control" placeholder="Full name">
+        <input id="name" name="form[name]" type="text" class="form-control" placeholder="Full name">
     </div>
     <div class="form-group">
-        <input type="email" class="form-control"  placeholder="Email Address">
+        <input id="email" name="form[email]" type="email" class="form-control"  placeholder="Email Address">
     </div>
-    <textarea class="form-control" placeholder="Enter your message here.."></textarea>
+    <textarea id="message" name="form[message]"class="form-control" placeholder="Enter your message here.."></textarea>
     <input type="submit" class="btn btn-default" value="Send Message">
-    <button type="button" class="btn btn-default">Clear Fields</button>
+    <button id="clear_form" type="button" class="btn btn-default">Clear Fields</button>
 </form>
+
+
+<script>
+    jQuery(document).ready(function($){
+        jQuery( "#contact-form" ).submit(function(event) {
+            event.preventDefault();
+        
+            var formData = {
+                'name'      : jQuery('input[id=name]').val(),
+                'email'     : jQuery('input[id=email]').val(),
+                'message'   : jQuery('textarea[id=message]').val()
+            };
+            
+            var data = {
+                'action': 'contact_log',
+                'log'   : formData
+            };
+            
+            jQuery.post('/wp-admin/admin-ajax.php', data, function(response){
+                console.log(response);
+            });
+        });
+        jQuery("#clear_form").click(function(){
+           jQuery('#name').val(''); 
+           jQuery('#email').val(''); 
+           jQuery('#message').val(''); 
+        });
+    });
+</script>
+   
