3094ff2836c1d55835d1d4d298d1b572a3fd5610
kiwisoju <glennjamesforrest@gmail.com>
2015-11-11 10:26:43 +0000

    Contact form

---
+++ wp-content/themes/industry/_contact_form.php
@@ -0,0 +1,12 @@
+<h2>Leave a Comment</h2>
+<form>
+    <div class="form-group">
+        <input type="text" class="form-control" placeholder="Full name">
+    </div>
+    <div class="form-group">
+        <input type="email" class="form-control"  placeholder="Email Address">
+    </div>
+    <textarea class="form-control" placeholder="Enter your message here.."></textarea>
+    <input type="submit" class="btn btn-default" value="Send Message">
+    <button type="button" class="btn btn-default">Clear Fields</button>
+</form>
